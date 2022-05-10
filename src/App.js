c4ffd819da9e8b1e2742ec150c663eb56b230575
ShaRaya <sharayascovil@u.northwestern.edu>
2022-05-10 01:35:03 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -22,4 +22,171 @@ function App() {
   );
 }
 
+function Button (props) {
+  return React.createElement(
+    "button",
+    { type: "submit" },
+    props.label
+  );
+}
+
+ReactDOM.render(
+  React.createElement(Button, { label: "Save"}),
+  mountNode
+);
+
+const Button = ({ label }) => (
+  <button type="submit">{label}</button>
+);
+
+const RandomValue = () => (
+  <div>
+    { Math.floor(Math.random() * 100) }
+  </div>
+);
+
+ReactDOM.render(<RandomValue />, mountNode);
+
+const ErrorDisplay = ({ message }) => (
+  <div style={ { color:'red', backgroundColor:'yellow' } }>
+    {message}
+  </div>
+);
+
+ReactDOM.render(
+  <ErrorDisplay
+    message="These aren't the droids you're looking for"
+  />,
+  mountNode
+);
+
+class ConditionalStyle extends React.Component {
+  render() {
+    return (
+      <div style={{ color: Math.random() < 0.5 ? 'green': 'red' }}>
+        How do you like this?
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ConditionalStyle />,
+  mountNode,
+);
+
+<ul>
+  {todos.map(todo =>
+    <li>{todo.body}</li>
+  )}
+</ul>
+
+React.createElement(
+  "ul",
+  null,
+  todos.map(todo =>
+    React.createElement("li", null, todo.body)
+  ),
+);
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button>{this.props.label}</button>
+    );
+  }
+}
+
+// Use it (same syntax)
+ReactDOM.render(<Button label="Save" />, mountNode);
+
+function Display(props) {
+  return (
+    <pre>COUNT VALUE HERE...</pre>
+  );
+}
+
+ReactDOM.render(<><Button /><Display /></>, mountNode);
+
+ReactDOM.render(
+  <div>
+    <Button />
+    <Display />
+  </div>,
+  mountNode
+);
+
+const CountManager = () => {
+  return (
+    <>
+      <Button />
+      <Display />
+    </>
+  );
+};
+
+ReactDOM.render(<CountManager />, mountNode);
+
+const Button = () => {
+  return (
+    <button onClick={() => console.log('TODO: Increment counter')}>
+      +1
+    </button>
+  );
+};
+
+const Display = ({ content }) => (
+  <pre>{content}</pre>
+);
+
+const CountManager = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Button />
+      <Display content={count} />
+    </>
+  );
+};
+
+ReactDOM.render(<CountManager />, mountNode);
+
+function CountManager() {
+  // ....
+  const incrementCounter = () => {
+    setCount(count + 1);
+  };
+
+  // ...
+};
+
+const Button = ({ clickAction }) => {
+  return (
+    <button onClick={clickAction}>
+      +1
+    </button>
+  );
+};
+
+// ...
+
+const CountManager = () => {
+  // ...
+
+  return (
+    <>
+      <Button clickAction={incrementCounter} clickValue={1} />
+      <Button clickAction={incrementCounter} clickValue={5} />
+      <Button clickAction={incrementCounter} clickValue={10} />
+      <Display content={count} />
+    </>
+  );
+
+};
+
+
+
+
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,7 +11,145 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const render = () => {
+
+document.getElementById('mountNode').innerHTML = `
+  <div>
+    Hello HTML
+  </div>
+`;
+
+ReactDOM.render(
+  React.createElement(
+    'div',
+    null,
+    'Hello React',
+  ),
+  document.getElementById('mountNode2'),
+);
+
+document.getElementById('mountNode').innerHTML = `
+  <div>
+    Hello HTML
+    <input />
+  </div>
+`;
+
+ReactDOM.render(
+  React.createElement(
+    "div",
+    null,
+    "Hello React ",
+    React.createElement("input")
+  ),
+  document.getElementById('mountNode2'),
+);
+
+document.getElementById('mountNode1').innerHTML = `
+  <div>
+    Hello HTML
+    <input />
+    <pre>${new Date().toLocaleTimeString()}</p>
+  </div>
+`;
+
+ReactDOM.render(
+  React.createElement(
+    'div',
+    null,
+    'Hello React ',
+    React.createElement('input'),
+    React.createElement(
+      'pre',
+      null,
+      new Date().toLocaleTimeString()
+    )
+  ),
+  document.getElementById('mountNode2')
+  );
+};
+
+const SearchEngines = ({ engines }) => {
+  return (
+    <List>
+      {engines.map(engine => <ClickableImage {...engine} />)}
+    </List>
+  );
+};
+
+ReactDOM.render(
+ <SearchEngines engines={data} />,
+ document.getElementById("mountNode")
+);
+
+const Button = () => {
+  let count = 0;
+
+  return (
+    <button>{count}</button>
+  );
+};
+
+ReactDOM.render(<Button />, mountNode);
+
+function Button() {
+  let count = 0;
+
+  return (
+    <button onClick={() => console.log('Button clicked')}>
+      {count}
+    </button>
+  );
+}
+
+ReactDOM.render(<Button />, mountNode);
+
+function func() {}
+
+<button onClick={func} />;
+
+const [count, setCount] = React.useState(0);
+
+const Button = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      {count}
+    </button>
+  );
+};
+
+ReactDOM.render(<Button />, mountNode);
+
+incrementCounter = (incrementValue) => {
+  setCount(count + incrementValue);
+};
+
+const Button = ({ clickValue, clickAction }) => {
+  return (
+    <button onClick={() => clickAction(clickValue)}>
+      +{clickValue}
+    </button>
+  );
+
+  const CharacterCounter = () => {
+    return (
+      <div>
+        <textarea cols={80} rows={10} />
+        <div>Count: X</div>
+      </div>
+    );
+  };
+  
+  ReactDOM.render(<CharacterCounter />, mountNode);
+
+
+useEffect(() => {
+  // Do something after each render
+  // but only if dep1 or dep2 changed
+}, [dep1, dep2]);
+
+};
+
 reportWebVitals();
